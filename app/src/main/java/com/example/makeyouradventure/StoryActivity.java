1aff3bd66385ff257a073cef6f7258a18ab540de
Kingsley Smith <ks3509944@gmail.com>
2020-05-04 18:16:33 -0700

    hi help

---
+++ app/src/main/java/com/example/makeyouradventure/StoryActivity.java
@@ -0,0 +1,22 @@
+package com.example.makeyouradventure;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class StoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+        Intent intent= getIntent();
+        String name =intent.getStringExtra("name");
+
+        Toast.makeText(this, name, Toast.LENGTH_LONG).show();
+
+    }
+}
